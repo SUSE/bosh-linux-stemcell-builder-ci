ae1aebf5c52cc2b648eeeff3c495b831cb51bfc0
Mauro Morales <mamorales@suse.com>
2018-04-11 16:30:46 +0200

    Modify prepare bats to use environments
    Signed-off-by: André Duffeck <aduffeck@suse.com>

--- tasks/bats/prepare-bats-config.sh
+++ tasks/bats/prepare-bats-config.sh
@@ -7,16 +7,18 @@ creds_path() { bosh-cli int director-state/director-creds.yml --path="$1" ; }
 
 source environment/metadata
 
+env=$(cat environment/name)
+
 cat > bats-config/bats.env <<EOF
 export BOSH_ENVIRONMENT=$BOSH_internal_ip
 export BOSH_CLIENT="admin"
 export BOSH_CLIENT_SECRET="$( creds_path /admin_password )"
 export BOSH_CA_CERT="$( creds_path /director_ssl/ca )"
-export BOSH_GW_HOST="$( state_path /instance_groups/name=bosh/networks/name=public/static_ips/0 2>/dev/null )"
+export BOSH_GW_HOST="$( state_path /instance_groups/name=bosh/networks/name=${env}/static_ips/0 2>/dev/null )"
 export BOSH_GW_USER="jumpbox"
 export BAT_PRIVATE_KEY="$( creds_path /jumpbox_ssh/private_key )"
 
-export BAT_DNS_HOST="$( state_path /instance_groups/name=bosh/networks/name=public/static_ips/0 2>/dev/null )"
+export BAT_DNS_HOST="$( state_path /instance_groups/name=bosh/networks/name=${env}/static_ips/0 2>/dev/null )"
 
 export BAT_INFRASTRUCTURE=openstack
 export BAT_NETWORKING=dynamic
