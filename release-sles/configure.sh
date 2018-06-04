9a95b46881bee7dfa43f8ee181c288c34533c28a
Mario Manno <mario.manno@suse.com>
2018-06-04 11:35:20 +0200

    Add SLES openstack pipeline

---
+++ release-sles/configure.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ev
+
+: ${TARGET:="suse.de"}
+: ${CONCOURSE_SECRETS_FILE:="../../cloudfoundry/secure/concourse-secrets.yml.gpg"}
+: ${TERRAFORM_DATA_DIR:="../../cloudfoundry/engcloud.prv.suse.net/terraform"}
+
+fly -t $TARGET set-pipeline \
+  -c pipeline.yml  \
+  -p bosh:release-sles:stemcells \
+  -l ../shared_vars.yml \
+  -l vars.yml \
+  -l <(gpg --decrypt --batch --no-tty "$CONCOURSE_SECRETS_FILE") \
+  -l $TERRAFORM_DATA_DIR/vars.yml \
+  -v openstack-private-key-data="`gpg --decrypt --batch --no-tty $TERRAFORM_DATA_DIR/bosh.pem.gpg`" \
+  $*
