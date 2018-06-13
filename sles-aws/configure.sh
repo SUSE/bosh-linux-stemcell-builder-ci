a3c1c377a8a97edda4df71fe2fe6534c7c813655
Mario Manno <mario.manno@suse.com>
2018-06-13 11:50:55 +0200

    Move secrets to credhub

--- sles-aws/configure.sh
+++ sles-aws/configure.sh
@@ -3,7 +3,6 @@
 set -ev
 
 : ${TARGET:="sles"}
-: ${CONCOURSE_SECRETS_FILE:="../../cloudfoundry/secure/concourse-secrets.yml.gpg"}
 
 fly -t $TARGET set-pipeline \
   -p bosh:release-sles:aws-bats \
@@ -11,5 +10,4 @@ fly -t $TARGET set-pipeline \
   -l vars.yml \
   -v aws-ssh-public-key="$(cat ../../cloudfoundry/aws/bats/sles-bats.pem.pub)" \
   -v aws-ssh-private-key="$(cat ../../cloudfoundry/aws/bats/sles-bats.pem)" \
-  -l <(gpg --decrypt --batch --no-tty "$CONCOURSE_SECRETS_FILE" 2> /dev/null) \
   $*
