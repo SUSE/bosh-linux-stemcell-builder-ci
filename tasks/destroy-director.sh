29d14bdb28e27192d0a3e4456625eed8527bca06
Mario Manno <mario.manno@suse.com>
2018-06-15 17:46:31 +0200

    Split delete-deployment into two tasks
    1. try to clean up the inner director
    2. remove inner director deployment from outer director

--- tasks/destroy-director.sh
+++ tasks/destroy-director.sh
@@ -1,16 +1,19 @@
 #!/usr/bin/env bash
 
-set -ev
+set -e
 
-export bosh_cli=$(realpath bosh-cli/bosh-cli-*)
-chmod +x $bosh_cli
+cp bosh-cli/bosh-cli-* /usr/local/bin/bosh-cli
+chmod +x /usr/local/bin/bosh-cli
 
-export BOSH_ENVIRONMENT=`$bosh_cli int suse-cf/engcloud.prv.suse.net/bosh/vars.yml --path /external_ip`
-export BOSH_CA_CERT=`$bosh_cli int suse-cf/engcloud.prv.suse.net/bosh/director-creds.yml --path /director_ssl/ca`
+state_path() { bosh-cli int director-state/director.yml --path="$1" ; }
+
+source environment/metadata
+export BOSH_ENVIRONMENT=$BOSH_internal_ip
+export BOSH_CA_CERT=`bosh-cli int director-state/director-creds.yml --path /director_ssl/ca`
 export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=`$bosh_cli int suse-cf/engcloud.prv.suse.net/bosh/director-creds.yml --path /admin_password`
+export BOSH_CLIENT_SECRET=`bosh-cli int director-state/director-creds.yml --path /admin_password`
 
-$bosh_cli -n delete-deployment -d $(cat environment/name) --force
-$bosh_cli -n delete-config --type cloud --name $(cat environment/name)
-$bosh_cli -n clean-up
-$bosh_cli disks --orphaned --json | jq -r '.Tables[].Rows[] .disk_cid' | xargs -rn1 $bosh_cli -n delete-disk
+set +e
+
+bosh-cli deployments --column name | xargs -n1 -I % bosh-cli -n -d % delete-deployment
+bosh-cli clean-up -n --all
