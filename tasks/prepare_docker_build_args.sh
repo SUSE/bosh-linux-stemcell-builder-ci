1bf68158ff097595bc529cac02065f7eb05ae733
André Duffeck <aduffeck@suse.com>
2019-02-15 13:33:14 +0100

    Add labels for the stemcell flavor and version

--- tasks/prepare_docker_build_args.sh
+++ tasks/prepare_docker_build_args.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 tag="$TAG_PREFIX`cat semver.os-image/number`"
+version=`cat versioned-stemcell/VERSION`
 
-echo "$BUILD_ARGS_TMPL" | sed "s/base_image_tag_var/$tag/" > build_args/json
+echo "$BUILD_ARGS_TMPL" | sed "s/base_image_tag_var/$tag/; s/version_var/$version/" > build_args/json
